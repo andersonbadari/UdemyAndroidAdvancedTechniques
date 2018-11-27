38733a33e4e9f05b400c70a893314020d8a2d999
Anderson <andybadari@gmail.com>
2018-11-26 23:33:06 -0200

    Detalhes

--- app/src/main/java/com/andersonbadari/udemyandroidadvancedtechniques/checkout/CheckoutActivity.kt
+++ app/src/main/java/com/andersonbadari/udemyandroidadvancedtechniques/checkout/CheckoutActivity.kt
@@ -2,6 +2,7 @@ package com.andersonbadari.udemyandroidadvancedtechniques.checkout
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
 import com.andersonbadari.udemyandroidadvancedtechniques.R
 import com.andersonbadari.udemyandroidadvancedtechniques.feed.model.OfferModel
 import kotlinx.android.synthetic.main.activity_checkout.*
@@ -15,7 +16,17 @@ class CheckoutActivity : AppCompatActivity() {
         val discount = intent?.getStringExtra("DISC")?:""
         val courseName = intent?.getStringExtra("NAME")?:""
         supportActionBar?.title = courseName
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         details.bindData(OfferModel(discount,courseName))
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+
+        if (item?.itemId == android.R.id.home) {
+            finish()
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/com/andersonbadari/udemyandroidadvancedtechniques/details/DetailsActivity.kt
+++ app/src/main/java/com/andersonbadari/udemyandroidadvancedtechniques/details/DetailsActivity.kt
@@ -2,6 +2,7 @@ package com.andersonbadari.udemyandroidadvancedtechniques.details
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
 import com.andersonbadari.udemyandroidadvancedtechniques.R
 import com.andersonbadari.udemyandroidadvancedtechniques.feed.model.OfferModel
 import kotlinx.android.synthetic.main.activity_details.*
@@ -15,7 +16,17 @@ class DetailsActivity : AppCompatActivity() {
         val discount = intent?.getStringExtra("DISC")?:""
         val courseName = intent?.getStringExtra("NAME")?:""
         supportActionBar?.title = courseName
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         details.bindData(OfferModel(discount,courseName))
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+
+        if (item?.itemId == android.R.id.home) {
+            finish()
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
