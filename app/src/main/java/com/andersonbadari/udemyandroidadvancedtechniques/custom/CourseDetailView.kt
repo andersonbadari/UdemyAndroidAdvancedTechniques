097f7c90bc96b7bd2b8a81c414187728dfaeeeec
Anderson <andybadari@gmail.com>
2018-11-26 23:29:38 -0200

    View customizada

--- app/src/main/java/com/andersonbadari/udemyandroidadvancedtechniques/custom/CourseDetailView.kt
+++ app/src/main/java/com/andersonbadari/udemyandroidadvancedtechniques/custom/CourseDetailView.kt
@@ -3,21 +3,22 @@ package com.andersonbadari.udemyandroidadvancedtechniques.custom
 import android.content.Context
 import android.support.constraint.ConstraintLayout
 import android.util.AttributeSet
-import android.view.LayoutInflater
 import android.view.View
+import android.widget.TextView
 import com.andersonbadari.udemyandroidadvancedtechniques.R
 import com.andersonbadari.udemyandroidadvancedtechniques.feed.model.OfferModel
-import kotlinx.android.synthetic.main.view_details.view.*
 
 class CourseDetailView @JvmOverloads constructor(
         context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : ConstraintLayout(context, attrs, defStyleAttr) {
 
-    private var root: View = LayoutInflater.from(context).inflate(R.layout.view_details, this, false)
+    init {
+        View.inflate(context, R.layout.view_details, this)
+    }
 
     fun bindData(offer: OfferModel) {
-        custom_course_name.text = offer.courseName
-        custom_course_discount.text = offer.discountPercentage
+        findViewById<TextView>(R.id.custom_course_name).text = offer.courseName
+        findViewById<TextView>(R.id.custom_course_discount).text = offer.discountPercentage
     }
 
 }
