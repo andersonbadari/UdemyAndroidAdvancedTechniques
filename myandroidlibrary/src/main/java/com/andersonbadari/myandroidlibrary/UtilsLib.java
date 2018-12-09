690ce53c9282a3aea98ed438137c5e4ef43c8596
Anderson <andybadari@gmail.com>
2018-12-09 12:47:04 -0200

    Add lib

---
+++ myandroidlibrary/src/main/java/com/andersonbadari/myandroidlibrary/UtilsLib.java
@@ -0,0 +1,9 @@
+package com.andersonbadari.myandroidlibrary;
+
+public class UtilsLib {
+
+    public static int sum(int firstValue, int secondValue) {
+        return firstValue + secondValue;
+    }
+
+}
